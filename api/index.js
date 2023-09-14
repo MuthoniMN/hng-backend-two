98daffdb9d435658b75247e34549b6a2c90511c2
MuthoniMN <105012834+MuthoniMN@users.noreply.github.com>
2023-09-15 01:14:03 +0300

    updating create

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ app.get('/api/:id', (req, res) => {
   Person.findOne({
     id: id
   })
-    .then((data) => res.json(data))
+    .then((data) => res.json(data || "Person not found"))
     .catch((err) => console.error(err))
 })
 
