94f972db869bcc71663acd9500618757a1833b84
MuthoniMN <105012834+MuthoniMN@users.noreply.github.com>
2023-09-14 17:48:54 +0300

    creating a schema

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ require('dotenv').config()
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const personSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    id:{
+        type: Number
+    }
+  })
+
 app.use(express.json())
 
 app.get('/api/:id', (req, res) => {
