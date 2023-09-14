4e4e4973d92d513e4ddd5b7b690907d0ce5aa199
MuthoniMN <105012834+MuthoniMN@users.noreply.github.com>
2023-09-14 18:08:58 +0300

    database interaction

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ require('dotenv').config()
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const generateId = () => {
+    return Math.max(...phoneBook.map((a) => a.id))++
+}
+
 const personSchema = new mongoose.Schema({
     name: {
       type: String,
@@ -21,21 +25,46 @@ app.use(express.json())
 
 app.get('/api/:id', (req, res) => {
     let id = req.params.id
+    Person.findOne({
+        id: id
+      })
+    .then((data) => res.json(data))
+    .catch((err) =>  console.error(err))
 })
 
 app.delete('/api/:id', (req, res) => {
     let id = req.params.id
+    Person.deleteOne({
+        id: id
+      })
+    .then((data) => res.json(data))
+    .catch((err) =>  console.error(err))
 })
 
 app.put('/api/:id', (req, res) => {
     let id = req.params.id
+    let personName = req.body.name
+    Person.findOneAndUpdate(
+        {
+            id: id
+        },
+        {
+            name: personName
+        },
+        { new: true }
+      )
+      .then((data) => res.json(data))
+      .catch((err) =>  console.error(err))
 })
 
-const generateId = () => {
-    return Math.max(...phoneBook.map((a) => a.id))++
-}
-
 app.post('/api/', (req, res) => {
-
+    let person = new Person({
+        name: req.body.name,
+        id: generateId(),
+      })
+      person.save((err, data) => {
+        if(err) console.error(err)
+        console.log("Saved!!!")
+      })
 })
 app.listen(3333, () => console.log("We Running Hunnnaaaayyyyy"))
